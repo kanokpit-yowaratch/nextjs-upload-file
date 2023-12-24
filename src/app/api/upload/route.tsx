1c8c77e78a94e4152bab5c5f5322321abe176dc6
Kanokpit Yowaratch <kanokpit.y@gmail.com>
2023-12-24 14:06:35 +0700

    Test access api post

--- src/app/api/upload/route.tsx
+++ src/app/api/upload/route.tsx
@@ -7,19 +7,20 @@ export async function POST(request: NextRequest) {
     const file: File | null = data.get('file') as unknown as File
     const dirRelativeToPublicFolder = "uploads";
     const dir = path.resolve("./", "public", dirRelativeToPublicFolder);
-    // console.log(dir);
+    console.log(dir);
+    return NextResponse.json({ path: dir })
 
-    if (!file) {
-        return NextResponse.json({ success: false })
-    }
+    // if (!file) {
+    //     return NextResponse.json({ success: false })
+    // }
 
-    const bytes = await file.arrayBuffer()
-    const buffer = Buffer.from(bytes)
+    // const bytes = await file.arrayBuffer()
+    // const buffer = Buffer.from(bytes)
 
-    const filePath = `${dir}/${file.name}`
-    await writeFile(filePath, buffer)
+    // const filePath = `${dir}/${file.name}`
+    // await writeFile(filePath, buffer)
 
-    return NextResponse.json({ success: true })
+    // return NextResponse.json({ success: true })
 }
 
 export async function GET() {
